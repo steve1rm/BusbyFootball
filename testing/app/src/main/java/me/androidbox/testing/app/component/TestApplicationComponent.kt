2ed6017522b68c2341bc9ce43ff1d99f22707ffd
Steve <steve@kitkat@gmail.com>
2021-05-30 16:59:19 +0700

    Adds getting the dependency for the FakeFootballService

--- testing/app/src/main/java/me/androidbox/testing/app/component/TestApplicationComponent.kt
+++ testing/app/src/main/java/me/androidbox/testing/app/component/TestApplicationComponent.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import dagger.BindsInstance
 import dagger.Component
 import me.androidbox.appcomponent.ApplicationComponent
+import me.androidbox.testing.app.data.FakeFootballServices
 import me.androidbox.testing.app.modules.TestDataModule
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [TestDataModule::class])
 interface TestApplicationComponent : ApplicationComponent {
+
+    fun getFakeFootballService(): FakeFootballServices
 
     @Component.Factory
     interface Factory {
